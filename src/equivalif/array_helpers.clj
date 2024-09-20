bf6d29123de337ade48472248a00137e3cf0799b
Jonathan Boccara <boccaraj@gmail.com>
2024-09-20 16:26:06 +0200

    Use recur in the implementation of alternate

--- src/equivalif/array_helpers.clj
+++ src/equivalif/array_helpers.clj
@@ -2,8 +2,10 @@
 
 (defn alternate
   "Returns an array that contains elements from both inputs alternatively, plus the remainder of the longest"
-  [array1 array2]
-  (cond (empty? array1) array2
-        (empty? array2) array1
-        :else (into [(first array1) (first array2)] (alternate (rest array1) (rest array2))))
+  ([array1 array2]
+   (alternate [] array1 array2))
+  ([result array1 array2]
+   (cond (empty? array1) (into result array2)
+         (empty? array2) (into result array1)
+         :else (recur (into result [(first array1) (first array2)]) (rest array1) (rest array2))))
   )
