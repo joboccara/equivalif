1f3a1911f4babf59d933f75308a4c36918ae1cce
Jonathan Boccara <boccaraj@gmail.com>
2024-10-06 23:40:08 +0200

    Style for inputs

--- src/equivalif/app.cljs
+++ src/equivalif/app.cljs
@@ -22,10 +22,10 @@
 
 (defn expressions-form [expressions]
      [:form
-       [:div
-         [:label {:for "expression1"} "Expression 1"] (text-input expressions :expression1)]
-       [:div
-         [:label {:for "expression2"} "Expression 2"] (text-input expressions :expression2)]])
+       [:div {:class "expression-input-with-label"}
+         [:label {:for "expression1" :class "expression-label"} "Compare"] (text-input expressions :expression1)]
+       [:div {:class "expression-input-with-label"}
+         [:label {:for "expression2" :class "expression-label"} "With"] (text-input expressions :expression2)]])
 
 (defn truth-table [expressions]
   (let [variables (evaluator/find-vars (:expression1 @expressions))
@@ -52,7 +52,8 @@
   [:div "The expressions don't contain the same variables"])
 
 (defn text-input [expressions kw]
-  [:input {:type "text"
-            :id (name kw)
-            :name (name kw)
-            :on-change #(swap! expressions assoc kw (-> % .-target .-value))}])
+  [:input {:class "expression-input"
+           :type "text"
+           :id (name kw)
+           :name (name kw)
+           :on-change #(swap! expressions assoc kw (-> % .-target .-value))}])
