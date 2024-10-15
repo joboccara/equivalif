559e67454e8bef7b24dc40e245649a7b08804a5e
Jonathan Boccara <boccaraj@gmail.com>
2024-10-15 14:24:21 +0200

    Add tests for evaluator

---
+++ test/equivalif/evaluator_test.clj
@@ -0,0 +1,26 @@
+(ns equivalif.evaluator-test
+ (:require [clojure.test :refer [deftest is testing]]
+           [equivalif.evaluator :refer [evaluate]]))
+
+(deftest evaluate-and
+  (testing
+   (let [ev #(evaluate '(and a b) %)]
+     (and (is (= false (ev {'a false 'b false})))
+          (is (= false (ev {'a false 'b true})))
+          (is (= false (ev {'a true 'b false})))
+          (is (= true (ev {'a true 'b true})))))))
+
+(deftest evaluate-or
+  (testing
+   (let [ev #(evaluate '(or a b) %)]
+     (and (is (= false (ev {'a false 'b false})))
+          (is (= true (ev {'a false 'b true})))
+          (is (= true (ev {'a true 'b false})))
+          (is (= true (ev {'a true 'b true})))))))
+
+(deftest evaluate-not
+  (testing
+   (let [ev #(evaluate '(not a) %)]
+     (and (is (= false (ev {'a true})))
+          (is (= true (ev {'a false})))))))
+
